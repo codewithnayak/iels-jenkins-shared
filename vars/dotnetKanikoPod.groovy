4d33245d733d968a6a7f1ced43738b40fe2856f6
codewithnayak <codewithnayak@gmail.com>
2023-03-18 19:35:38 +0000

    dotnet with kaniko pod added as jenkins agent

---
+++ vars/dotnetKanikoPod.groovy
@@ -0,0 +1,38 @@
+/*
+* This is the .net build pod template 
+* dotnetBuildPod()
+* New parameters will be added later 
+*/
+def call(){
+
+yaml = '''
+    apiVersion: v1
+    kind: Pod
+    spec:
+      containers:
+      - name: dotnetcore
+        image: mcr.microsoft.com/dotnet/sdk:6.0
+        command:
+        - sleep
+        args:
+        - 99d
+      - name: kaniko
+        image: gcr.io/kaniko-project/executor:debug
+        command:
+        - sleep
+        args:
+        - 9999999
+        volumeMounts:
+        - name: kaniko-secret
+          mountPath: /kaniko/.docker
+      restartPolicy: Never
+      volumes:
+      - name: kaniko-secret
+        secret:
+            secretName: dockercred
+            items:
+            - key: .dockerconfigjson
+              path: config.json
+'''
+return yaml
+}
