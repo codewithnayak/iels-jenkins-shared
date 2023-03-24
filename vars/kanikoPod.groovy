3b956b06076a447510e640724f7379f08d1ab983
codewithnayak <codewithnayak@gmail.com>
2023-03-24 10:54:46 +0000

    simple kaniko pod added

---
+++ vars/kanikoPod.groovy
@@ -0,0 +1,27 @@
+def call(){
+
+yaml = '''
+    apiVersion: v1
+    kind: Pod
+    spec:
+      containers:
+      - name: kaniko
+        image: gcr.io/kaniko-project/executor:debug
+        command:
+        - sleep
+        args:
+        - 9999999
+        volumeMounts:
+        - name: kaniko-secret
+          mountPath: /kaniko/.docker
+      restartPolicy: Never
+      volumes:
+      - name: kaniko-secret
+        secret:
+            secretName: dockercred
+            items:
+            - key: .dockerconfigjson
+              path: config.json
+'''
+return yaml
+}
